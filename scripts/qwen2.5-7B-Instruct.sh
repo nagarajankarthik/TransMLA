69fd5a7e001c0a71771fdf1b0c3800e88aa4e53b
Nagarajan Karthik <nkarthik@aisingapore.org>
2025-11-27 18:36:03 +0800

    Added path to cache directories.

--- scripts/qwen2.5-7B-Instruct.sh
+++ scripts/qwen2.5-7B-Instruct.sh
@@ -1,3 +1,5 @@
+export HF_HOME=/data/projects/71001002/nkarthik/cache_karthik
+export TRANFORMERS_CACHE=/data/projects/71001002/nkarthik/cache_karthik
 model_path=Qwen/Qwen2.5-7B-Instruct
 save_path=outputs/qwen2_5-7B-Instruct-deepseek
 eval_batch_size=8
