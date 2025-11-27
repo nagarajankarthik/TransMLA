90172534a5b54f4ac104f8e4d97df4b5914a8d40
Nagarajan Karthik <nkarthik@aisingapore.org>
2025-11-27 18:35:18 +0800

    Modified options.

--- scripts/qwen3-4B.sh
+++ scripts/qwen3-4B.sh
@@ -1,3 +1,5 @@
+export HF_HOME=/data/projects/71001002/nkarthik/cache_karthik
+export TRANFORMERS_CACHE=/data/projects/71001002/nkarthik/cache_karthik
 model_path=Qwen/Qwen3-4B
 save_path=outputs/qwen3-4B-deepseek-qkv-norm
 eval_batch_size=8
@@ -5,11 +7,12 @@ eval_batch_size=8
 python3 transmla/converter.py \
     --model-path $model_path \
     --save-path $save_path \
+    --cal-dataset wikitext2 \
     --dtype bf16 \
-    --device cuda \
+    --device cuda:0 \
     --ppl-eval-batch-size $eval_batch_size \
-    --freqfold auto \
-    --collapse auto \
+    --freqfold 2 \
+    --collapse 1 \
     --qk-mqa-dim 64 \
     --q-lora-rank 512 \
     --kv-lora-rank 512 \
