8ab6e872c371eb688e1b1f0ddc1149f0d0cfd499
Nagarajan Karthik <nkarthik@hopper-l-03.cm.cluster>
2025-12-09 09:22:49 +0800

    Changed collapse parameter value from 1 to 2 in an attempt to reproduce lowest perplexity score.

--- scripts/qwen3-4B.sh
+++ scripts/qwen3-4B.sh
@@ -1,5 +1,5 @@
-export HF_HOME=/data/projects/71001002/nkarthik/cache_karthik
-export TRANFORMERS_CACHE=/data/projects/71001002/nkarthik/cache_karthik
+export HF_HOME=/scratch_aisg/SPEC-SF-AISG/cache/huggingface
+export TRANFORMERS_CACHE=/scratch_aisg/SPEC-SF-AISG/cache
 model_path=Qwen/Qwen3-4B
 save_path=outputs/qwen3-4B-deepseek-qkv-norm
 eval_batch_size=8
@@ -12,7 +12,7 @@ python3 transmla/converter.py \
     --device cuda:0 \
     --ppl-eval-batch-size $eval_batch_size \
     --freqfold 2 \
-    --collapse 1 \
+    --collapse 2 \
     --qk-mqa-dim 64 \
     --q-lora-rank 512 \
     --kv-lora-rank 512 \
